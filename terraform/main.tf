3f27d52dffd1dd0d71f2fc0846594f5be107e3f3
toyamagu <toyamagu2021@gmail.com>
2022-10-03 18:12:57 +0900

    modifyu

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,6 +66,7 @@ resource "aws_iam_role_policy_attachment" "github_actions" {
 resource "aws_ecr_repository" "foo" {
   name                 = var.ecr_repo_name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = false
